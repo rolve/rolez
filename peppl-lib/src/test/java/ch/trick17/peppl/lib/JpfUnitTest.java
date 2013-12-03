b4c0e3e41e03da8d920f9e4308f8af759f94cec2
Michael Faes <rolve@trick17.ch>
2013-12-03 13:44:31 +0100

    JPF unit testing now working (using manual command line argument construction...)

--- peppl-lib/src/test/java/ch/trick17/peppl/lib/JpfUnitTest.java
+++ peppl-lib/src/test/java/ch/trick17/peppl/lib/JpfUnitTest.java
@@ -2,14 +2,44 @@ package ch.trick17.peppl.lib;
 
 import gov.nasa.jpf.util.test.TestJPF;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class JpfUnitTest extends TestJPF {
     
+    private static String[] args;
+    
+    @BeforeClass
+    public static void setup() throws IOException {
+        final Properties props = new Properties();
+        props.load(JpfUnitTest.class.getResourceAsStream("jpf.properties"));
+        props.setProperty("classpath", "lib/jpf-classes.jar"
+                + File.pathSeparator + "target/test-classes");
+        
+        final ArrayList<String> argList = new ArrayList<>();
+        for(final Entry<Object, Object> entry : props.entrySet())
+            argList.add("+" + entry.getKey() + "=" + entry.getValue());
+        
+        args = argList.toArray(new String[argList.size()]);
+    }
+    
     @Test
-    public void test() {
-        if(verifyNoPropertyViolation()) {
-            System.out.println("Works?");
+    public void testPass() {
+        if(verifyNoPropertyViolation(args)) {
+            assertTrue(true);
+        }
+    }
+    
+    @Test(expected = AssertionError.class)
+    public void testFail() {
+        if(verifyNoPropertyViolation(args)) {
+            assertTrue(false);
         }
     }
 }
