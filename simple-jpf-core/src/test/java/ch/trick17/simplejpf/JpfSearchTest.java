04eea30292711fbbce1cce3d94479ff5e11f5950
Michael Faes <rolve@trick17.ch>
2014-01-14 08:43:57 +0100

    Created a second test to demonstrate the JPF bug

--- simple-jpf-core/src/test/java/ch/trick17/simplejpf/JpfSearchTest.java
+++ simple-jpf-core/src/test/java/ch/trick17/simplejpf/JpfSearchTest.java
@@ -2,19 +2,11 @@ package ch.trick17.simplejpf;
 
 import gov.nasa.jpf.Config;
 import gov.nasa.jpf.JPF;
-import gov.nasa.jpf.ListenerAdapter;
-import gov.nasa.jpf.jvm.bytecode.GETFIELD;
-import gov.nasa.jpf.search.Search;
-import gov.nasa.jpf.vm.ChoiceGenerator;
-import gov.nasa.jpf.vm.FieldInfo;
-import gov.nasa.jpf.vm.Instruction;
-import gov.nasa.jpf.vm.MethodInfo;
-import gov.nasa.jpf.vm.ThreadInfo;
-import gov.nasa.jpf.vm.VM;
-import gov.nasa.jpf.vm.choice.ThreadChoiceFromSet;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.locks.LockSupport;
@@ -31,15 +23,14 @@ public class JpfSearchTest {
         final Config config = JPF.createConfig(new String[0]);
         config.load(JpfTest.class.getResourceAsStream("jpf.properties"));
         config.setProperty("classpath", System.getProperty("java.class.path"));
-        config.setTarget(Test.class.getName());
+        config.setTarget(Test2.class.getName());
         
-        final Map<String, String> results = new HashMap<>();
+        final Map<String, String> results = new LinkedHashMap<>();
         for(final String search : new String[]{DFSEARCH, INTERLEAVING}) {
             config.setProperty("search.class", search);
             config.printEntries();
             
             final JPF jpf = new JPF(config);
-            jpf.addListener(new TestListener());
             jpf.run();
             if(jpf.foundErrors())
                 results.put(search, jpf.getLastError().getDescription());
@@ -52,7 +43,7 @@ public class JpfSearchTest {
             System.out.println(result.getKey() + ": " + result.getValue());
     }
     
-    public static class Test {
+    public static class Test1 {
         
         public static void main(final String[] args) {
             final Thread main = Thread.currentThread();
@@ -94,41 +85,43 @@ public class JpfSearchTest {
         }
     }
     
-    static class TestListener extends ListenerAdapter {
-        @Override
-        public void instructionExecuted(final VM vm, final ThreadInfo thread,
-                final Instruction next, final Instruction instr) {
-            final MethodInfo method = thread.getTopFrameMethodInfo();
-            if(method != null) {
-                final String pkg = method.getClassInfo().getPackageName();
-                if(pkg.startsWith("ch.trick17.simplejpf")) {
-                    System.out.println(method.getFullName() + ": " + next);
-                    if(next.toString().contains("new java.lang.AssertionError"))
-                        System.currentTimeMillis();
+    public static class Test2 {
+        
+        public static void main(final String[] args) {
+            final Owned o = new Owned();
+            
+            o.prevOwners.addFirst(o.owner);
+            o.owner = null;
+            new Thread() {
+                @Override
+                public void run() {
+                    o.owner = Thread.currentThread();
+                    
+                    o.prevOwners.addFirst(o.owner);
+                    o.owner = null;
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            o.owner = o.prevOwners.removeFirst();
+                            LockSupport.unpark(o.owner);
+                        }
+                    }.start();
+                    
+                    while(o.owner != Thread.currentThread())
+                        LockSupport.park();
+                    
+                    o.owner = o.prevOwners.removeFirst();
+                    LockSupport.unpark(o.owner);
                 }
-            }
+            }.start();
+            
+            while(o.owner != Thread.currentThread())
+                LockSupport.park();
         }
         
-        @Override
-        public void choiceGeneratorSet(final VM vm,
-                final ChoiceGenerator<?> newCG) {
-            if(newCG instanceof ThreadChoiceFromSet) {
-                final ThreadChoiceFromSet threadChoice = (ThreadChoiceFromSet) newCG;
-                if(threadChoice.getId().contains("SHARED_FIELD_ACCESS")) {
-                    final Instruction insn = threadChoice.getInsn();
-                    if(insn instanceof GETFIELD) {
-                        final FieldInfo field = ((GETFIELD) insn)
-                                .getFieldInfo();
-                        if(field.getName().contains("blocked"))
-                            System.out.println("-- " + threadChoice);
-                    }
-                }
-            }
-        }
-        
-        @Override
-        public void stateBacktracked(final Search search) {
-            System.out.println("------- Backtracked -------");
+        static class Owned {
+            volatile Thread owner = Thread.currentThread();
+            final Deque<Thread> prevOwners = new ArrayDeque<Thread>();
         }
     }
 }
