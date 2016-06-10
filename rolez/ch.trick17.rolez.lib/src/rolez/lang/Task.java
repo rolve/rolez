18616aa20697c089cb8019b4bd239d0aaf5d3b59
Michael Faes <rolve@trick17.ch>
2016-06-10 09:58:17 +0200

    Reduced coupling between Task and TaskSystem by moving the stack of tasks into Task
    * Also, makes a Task.currentTask() method possible, much like Thread.currentThread()

--- rolez/ch.trick17.rolez.lib/src/rolez/lang/Task.java
+++ rolez/ch.trick17.rolez.lib/src/rolez/lang/Task.java
@@ -1,5 +1,8 @@
 package rolez.lang;
 
+import static java.lang.Thread.currentThread;
+
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
@@ -7,8 +10,24 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 
 public class Task<V> implements Runnable {
+    /**
+     * Keeps track of the tasks that are executed within a thread. Whenever a new task is started,
+     * it is added as a child to current, i.e., the top task on the stack.
+     */
+    private static final transient ThreadLocal<Deque<Task<?>>> localStack = new ThreadLocal<Deque<Task<?>>>() {
+        @Override
+        protected Deque<Task<?>> initialValue() {
+            return new ArrayDeque<>();
+        }
+    };
+    // IMPROVE: Could replace thread-local variable with "current" task variable
+    // that is passed to all generated methods. Wouldn't support call-backs
+    // from mapped methods though...
     
-    private final TaskSystem taskSystem;
+    public static Task<?> currentTask() {
+        return localStack.get().peek();
+    }
+    
     private Callable<V> callable;
     private final Sync sync = new Sync();
     
@@ -18,35 +37,33 @@ public class Task<V> implements Runnable {
     /**
      * The list of child tasks. Before a task finishes, it waits for all its children to finish.
      */
-    private final List<Task<?>> childTasks = new ArrayList<>();
+    private final List<Task<?>> children = new ArrayList<>();
     
-    void addChild(final Task<?> task) {
-        childTasks.add(task);
-    }
-    
-    Task(final Callable<V> callable, final TaskSystem system) {
+    Task(Callable<V> callable) {
         this.callable = callable;
-        this.taskSystem = system;
+        Task<?> parent = currentTask();
+        if(parent != null)
+            parent.children.add(this);
     }
     
     public final void run() {
-        Deque<Task<?>> localStack = taskSystem.localStack.get();
-        localStack.push(this);
+        Deque<Task<?>> stack = localStack.get();
+        stack.push(this);
         try {
             result = callable.call();
             /* Wait for child tasks to finish so that exceptions get propagated up the task stack */
-            for(final Task<?> task : childTasks)
+            for(final Task<?> task : children)
                 task.get();
         } catch(Throwable e) {
             /* Uncomment the following to print the exception as soon as the task is finished, in
              * case the parent task does not finish (e.g. because of a deadlock) and the exception
              * is not propagated. */
-            // java.lang.System.err.print("[" + Thread.currentThread().getName() + "] ");
-            // e.printStackTrace();
+            java.lang.System.err.print("[" + currentThread().getName() + "] ");
+            e.printStackTrace();
             result = null;
             exception = e;
         }
-        localStack.pop();
+        stack.pop();
         callable = null;
         
         /* Unblock threads waiting to get the result */
--- rolez/ch.trick17.rolez.lib/src/rolez/lang/TaskSystem.java
+++ rolez/ch.trick17.rolez.lib/src/rolez/lang/TaskSystem.java
@@ -1,43 +1,21 @@
 package rolez.lang;
 
 import java.io.Serializable;
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.concurrent.Callable;
 
 public abstract class TaskSystem implements Serializable {
     
-    /**
-     * Keeps track of the tasks that are executed within a thread. Whenever a new task is started,
-     * it is added as a child to current, i.e., the top task on the stack.
-     */
-    final transient ThreadLocal<Deque<Task<?>>> localStack = new ThreadLocal<Deque<Task<?>>>() {
-        @Override
-        protected Deque<Task<?>> initialValue() {
-            return new ArrayDeque<>();
-        }
-    };
-    // IMPROVE: Could replace thread-local variable with "current" task variable
-    // that is passed to all generated methods. Wouldn't support call-backs
-    // from mapped methods though...
-    
     public <V> Task<V> start(final Callable<V> callable) {
-        return start(new Task<>(callable, this));
+        final Task<V> task = new Task<>(callable);
+        doStart(task);
+        return task;
     }
     
     public <V> V run(final Callable<V> callable) {
-        Task<V> task = new Task<>(callable, this);
+        Task<V> task = new Task<>(callable);
         task.run();
         /* Propagate exceptions */
         return task.get();
-    }
-    
-    private <V> Task<V> start(final Task<V> task) {
-        final Task<?> current = localStack.get().peek();
-        if(current != null)
-            current.addChild(task);
-        doStart(task);
-        return task;
     }
     
     abstract void doStart(final Task<?> task);
