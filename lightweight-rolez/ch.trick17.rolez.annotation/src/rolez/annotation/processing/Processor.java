32462349390dd020f425b4ee2f9f755b3984d649
micgiger <micgiger@student.ethz.ch>
2017-06-25 12:39:27 +0200

    extracted ParameterTypeVisitor

--- lightweight-rolez/ch.trick17.rolez.annotation/src/rolez/annotation/processing/Processor.java
+++ lightweight-rolez/ch.trick17.rolez.annotation/src/rolez/annotation/processing/Processor.java
@@ -15,24 +15,12 @@ import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
-import javax.lang.model.type.ArrayType;
-import javax.lang.model.type.DeclaredType;
-import javax.lang.model.type.ErrorType;
-import javax.lang.model.type.ExecutableType;
-import javax.lang.model.type.NoType;
-import javax.lang.model.type.NullType;
-import javax.lang.model.type.PrimitiveType;
 import javax.lang.model.type.TypeMirror;
-import javax.lang.model.type.TypeVariable;
-import javax.lang.model.type.TypeVisitor;
-import javax.lang.model.type.UnionType;
-import javax.lang.model.type.WildcardType;
 import javax.tools.Diagnostic.Kind;
 
 import rolez.annotation.Readonly;
 import rolez.annotation.Readwrite;
 import rolez.annotation.Roleztask;
-import rolez.annotation.Guarded;
 
 @SupportedAnnotationTypes({"rolez.annotation.Roleztask","rolez.annotation.Readonly","rolez.annotation.Readwrite","rolez.annotation.Guarded"})
 @SupportedSourceVersion(SourceVersion.RELEASE_7)
@@ -126,79 +114,5 @@ public class Processor extends AbstractProcessor {
 	
 	private boolean isOfBooleanType(Element element) {
 		return element.asType().toString().equals("boolean");
-	}
-	
-	
-	private class ParameterTypeVisitor implements TypeVisitor<Message,Void> {
-		@Override
-		public Message visit(TypeMirror t, Void p) {
-			return null;
-		}
-
-		@Override
-		public Message visit(TypeMirror t) {
-			return null;
-		}
-
-		@Override
-		public Message visitPrimitive(PrimitiveType t, Void p) {
-			return null;
-		}
-
-		@Override
-		public Message visitNull(NullType t, Void p) {
-			return null;
-		}
-
-		@Override
-		public Message visitArray(ArrayType t, Void p) {
-			return null;
-		}
-
-		@Override
-		public Message visitDeclared(DeclaredType t, Void p) {
-			Element declaredType = t.asElement();
-			Guarded annotation = declaredType.getAnnotation(Guarded.class);
-			if (annotation == null) {
-				return new Message(Kind.ERROR, "Parameter has to be a guarded Type (i.e. annotated with @Guarded).");
-			}
-			
-			return null;
-		}
-
-		@Override
-		public Message visitError(ErrorType t, Void p) {
-			return null;
-		}
-
-		@Override
-		public Message visitTypeVariable(TypeVariable t, Void p) {
-			return null;
-		}
-
-		@Override
-		public Message visitWildcard(WildcardType t, Void p) {
-			return null;
-		}
-
-		@Override
-		public Message visitExecutable(ExecutableType t, Void p) {
-			return null;
-		}
-
-		@Override
-		public Message visitNoType(NoType t, Void p) {
-			return null;
-		}
-
-		@Override
-		public Message visitUnknown(TypeMirror t, Void p) {
-			return null;
-		}
-
-		@Override
-		public Message visitUnion(UnionType t, Void p) {
-			return null;
-		}
 	}
 }
