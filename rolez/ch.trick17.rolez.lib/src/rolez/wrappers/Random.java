65d6c3c54fcc52c6fd1d226a2b2fd260de0b76c5
micgiger <micgiger@student.ethz.ch>
2017-06-26 17:24:47 +0200

    created wrapper for Random class
    - is used to test features in AnnotationTest project

---
+++ rolez/ch.trick17.rolez.lib/src/rolez/wrappers/Random.java
@@ -0,0 +1,52 @@
+package rolez.wrappers;
+
+import rolez.lang.Guarded;
+
+public class Random extends Guarded {
+	
+	private java.util.Random random;
+	
+	public Random() {
+		random = new java.util.Random();
+	}
+	
+	public Random(long seed) {
+		random = new java.util.Random(seed);
+	}
+	
+	public boolean nextBoolean() {
+		return random.nextBoolean();
+	}
+	
+	public void nextBytes(byte[] bytes) {
+		random.nextBytes(bytes);
+	}
+	
+	public double nextDouble() {
+		return random.nextDouble();
+	}
+	
+	public float nextFloat() {
+		return random.nextFloat();
+	}
+	
+	public double nextGaussian() {
+		return random.nextGaussian();
+	}
+	
+	public int nextInt() {
+		return random.nextInt();
+	}
+	
+	public int nextInt(int n) {
+		return random.nextInt(n);
+	}
+	
+	public long nextLong() {
+		return random.nextLong();
+	}
+	
+	public void setSeed(long seed) {
+		random.setSeed(seed);
+	}
+}
