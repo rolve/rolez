6e2e358a6db421a6edf6c46bf7d09df05a61a303
micgiger <micgiger@student.ethz.ch>
2017-08-19 16:57:25 +0200

    testclass for the last commit

--- lightweight-rolez/ch.trick17.rolez.checked.transformer/src/main/java/test/Test.java
+++ lightweight-rolez/ch.trick17.rolez.checked.transformer/src/main/java/test/Test.java
@@ -4,6 +4,7 @@ import rolez.annotation.Checked;
 import rolez.annotation.Readonly;
 import rolez.annotation.Readwrite;
 import rolez.annotation.Roleztask;
+import rolez.checked.lang.Task;
 
 @Checked
 public class Test {
@@ -12,15 +13,47 @@ public class Test {
 	B field2;
 	
 	public static void main(String[] args) {
-		Test test = new Test();
-		test.testOriginal(new A(), new A(), true);
+		int i = 1;
+    	i++;
+    	new A(i);
 	}
+	
+	public Task<Void> $mainTask1() {
+        return new Task<Void>(new Object[]{this}, new Object[]{}, new Object[]{}) {
+            @Override
+            protected java.lang.Void runRolez() {
+            	int i = 1;
+            	i++;
+            	new A(i);
+                return null;
+            }
+        };
+    }
 
+	public void testOriginalNotRolez(A src, A dst, final boolean $asTask) {
+		dst.foo = foo(true);
+		int i = src.foo;
+		dst.foo = dst.foo + i;
+	}
+	
 	@Roleztask
 	public void testOriginal(@Readonly A src, @Readwrite A dst, final boolean $asTask) {
 		dst.foo = foo(true);
-		System.out.println(src.foo);
-		System.out.println(dst.foo);
+		int i = src.foo;
+		dst.foo = dst.foo + i;
+	}
+	
+	public Task<Void> testOriginalTask(final A src, final A dst, boolean $asTask) {
+		
+		return new Task<Void>(new Object[]{dst}, new Object[]{src}, new Object[]{}) {
+	        @Override
+	        protected Void runRolez() {
+	        	dst.foo = foo(true);
+	    		int i = src.foo;
+	    		dst.foo = dst.foo + i;
+	    		return null;
+	        }
+	    };
 	}
 	
 	@Roleztask
