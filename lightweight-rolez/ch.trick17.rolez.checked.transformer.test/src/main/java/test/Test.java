644804793773da469275b93f9524835f83a22c95
micgiger <micgiger@student.ethz.ch>
2017-08-22 23:54:28 +0200

    a compiling version of the return value problem inside the test project

--- lightweight-rolez/ch.trick17.rolez.checked.transformer.test/src/main/java/test/Test.java
+++ lightweight-rolez/ch.trick17.rolez.checked.transformer.test/src/main/java/test/Test.java
@@ -1,37 +1,88 @@
 package test;
 
+import java.util.Random;
+
 import rolez.annotation.Checked;
 import rolez.annotation.Readonly;
 import rolez.annotation.Readwrite;
 import rolez.annotation.Roleztask;
+import rolez.checked.internal.Tasks;
+import rolez.checked.lang.Task;
+import rolez.checked.lang.TaskSystem;
 
 @Checked
 public class Test {
 
-	A field1;
-	B field2;
-	
 	public static void main(String[] args) {
-		int i = 1;
-    	i++;
-    	new A(i);
+		
+	}
+	
+	public void test() {
+		Random r = new Random();
+		boolean b = r.nextBoolean(); // Could be everything we don't know at compile/transform-time
+		int i = bar(b);
+		int j = i;
 	}
 
-	public void testOriginalNotRolez(A src, A dst, final boolean $asTask) {
-		dst.foo = foo(true);
+	@Roleztask
+	public int bar(final boolean $asTask) {
+		int i = 1;
+		return i;
+	}
+
+	public void testTransformed() {
+		Task<Integer> t = null;
+		int i = 0;
+		
+		Random r = new Random();
+		boolean b = r.nextBoolean();
+		
+		if (b) {
+			t = (Task<Integer>) barTransformed(true);
+		} else {
+			i = (Integer) barTransformed(false);
+		}
+		
+		if (b) {
+			i = t.get();
+		}
+		int j = i;
+	}
+
+	public Object barTransformed(final boolean $asTask) {
+		if (!$asTask) {
+			int i = 1;
+			return i;
+		} 
+		
+		final Tasks $tasks = new Tasks();
+		try {
+			return $tasks.addInline(TaskSystem.getDefault().start(this.$barTask()));
+		}
+		finally {
+			$tasks.joinAll();
+		}
+	}
+	
+	public Task<Integer> $barTask() {
+        return new Task<Integer>(new Object[]{}, new Object[]{}, new Object[]{}) {
+            @java.lang.Override
+            protected Integer runRolez() {
+    			int i = 1;
+                return i;
+            }
+        };
+    }
+	
+	@Roleztask
+	public void foo(@Readonly A src, @Readwrite A dst, final boolean $asTask) {
+		dst.foo = foo1(true);
 		int i = src.foo;
 		dst.foo = dst.foo + i;
 	}
 	
 	@Roleztask
-	public void testOriginal(@Readonly A src, @Readwrite A dst, final boolean $asTask) {
-		dst.foo = foo(true);
-		int i = src.foo;
-		dst.foo = dst.foo + i;
-	}
-	
-	@Roleztask
-	public int foo(final boolean $asTask) {
+	public int foo1(final boolean $asTask) {
 		int i = 42;
 		return i;
 	}
