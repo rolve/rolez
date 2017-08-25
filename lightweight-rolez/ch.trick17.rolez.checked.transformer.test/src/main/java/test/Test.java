5b42175e30d058b48298e02864a657ece4d9f2d3
micgiger <micgiger@student.ethz.ch>
2017-08-25 12:59:09 +0200

    insert checking methods to checked field reads/writes

--- lightweight-rolez/ch.trick17.rolez.checked.transformer.test/src/main/java/test/Test.java
+++ lightweight-rolez/ch.trick17.rolez.checked.transformer.test/src/main/java/test/Test.java
@@ -1,14 +1,6 @@
 package test;
 
-import java.util.Random;
-
 import rolez.annotation.Checked;
-import rolez.annotation.Readonly;
-import rolez.annotation.Readwrite;
-import rolez.annotation.Roleztask;
-import rolez.checked.internal.Tasks;
-import rolez.checked.lang.Task;
-import rolez.checked.lang.TaskSystem;
 
 @Checked
 public class Test {
@@ -17,73 +9,9 @@ public class Test {
 		
 	}
 	
-	public void test() {
-		Random r = new Random();
-		boolean b = r.nextBoolean(); // Could be everything we don't know at compile/transform-time
-		int i = bar(b);
-		int j = i;
-	}
-
-	@Roleztask
-	public int bar(final boolean $asTask) {
-		int i = 1;
-		return i;
-	}
-
-	public void testTransformed() {
-		Task<Integer> t = null;
-		int i = 0;
-		
-		Random r = new Random();
-		boolean b = r.nextBoolean();
-		
-		if (b) {
-			t = (Task<Integer>) barTransformed(true);
-		} else {
-			i = (Integer) barTransformed(false);
-		}
-		
-		if (b) {
-			i = t.get();
-		}
-		int j = i;
-	}
-
-	public Object barTransformed(final boolean $asTask) {
-		if (!$asTask) {
-			int i = 1;
-			return i;
-		} 
-		
-		final Tasks $tasks = new Tasks();
-		try {
-			return $tasks.addInline(TaskSystem.getDefault().start(this.$barTask()));
-		}
-		finally {
-			$tasks.joinAll();
-		}
-	}
-	
-	public Task<Integer> $barTask() {
-        return new Task<Integer>(new Object[]{}, new Object[]{}, new Object[]{}) {
-            @java.lang.Override
-            protected Integer runRolez() {
-    			int i = 1;
-                return i;
-            }
-        };
-    }
-	
-	@Roleztask
-	public void foo(@Readonly A src, @Readwrite A dst, final boolean $asTask) {
-		dst.foo = foo1(true);
-		int i = src.foo;
-		dst.foo = dst.foo + i;
-	}
-	
-	@Roleztask
-	public int foo1(final boolean $asTask) {
-		int i = 42;
-		return i;
+	void test() {
+		A a = new A();	// Checked object
+		a.foo = 2;
+		int i = a.foo;
 	}
 }
--- lightweight-rolez/ch.trick17.rolez.checked.transformer/src/main/java/rolez/checked/transformer/CheckingTransformer.java
+++ lightweight-rolez/ch.trick17.rolez.checked.transformer/src/main/java/rolez/checked/transformer/CheckingTransformer.java
@@ -1,19 +1,182 @@
 package rolez.checked.transformer;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import rolez.checked.transformer.util.Constants;
 import soot.Body;
 import soot.BodyTransformer;
+import soot.Local;
+import soot.Scene;
+import soot.SootClass;
+import soot.Type;
+import soot.Unit;
+import soot.Value;
+import soot.jimple.AssignStmt;
+import soot.jimple.InstanceFieldRef;
+import soot.jimple.Jimple;
+import soot.util.Chain;
 
+/**
+ * Transformer inserts methods {@link rolez.checked.lang.Checked#checkLegalRead(rolez.checked.lang.Checked) checkLegalRead} to checked
+ * field reads and {@link rolez.checked.lang.Checked#checkLegalWrite(rolez.checked.lang.Checked) checkLegalWrite} to checked field writes.
+ * 
+ * @author Michael Giger
+ *
+ */
 public class CheckingTransformer extends BodyTransformer {
 	
 	static final Logger logger = LogManager.getLogger(CheckingTransformer.class);
 	
+	static final Jimple J = Jimple.v();
+	
+	int tempLocalCount = 0;
+	
+	Chain<Local> locals;
+	Chain<Unit> units;
+	
 	@Override
 	protected void internalTransform(Body b, String phaseName, Map options) {
 		logger.debug("Transforming " + b.getMethod().getDeclaringClass() + ":" + b.getMethod().getSignature());
+		
+		this.locals = b.getLocals();
+		this.units = b.getUnits();
+		
+		List<AssignStmt> fieldReads = findCheckedFieldReads();
+		List<AssignStmt> fieldWrites = findCheckedFieldWrites();
+		
+		for (AssignStmt read : fieldReads) {
+			addCheckLegalRead(read);
+		}
+		
+		for (AssignStmt write: fieldWrites) {
+			addCheckLegalWrite(write);
+		}
+		
+		logger.debug("\n" + b);
+		
+		// Reset tempLocalCount for next method
+		tempLocalCount = 0;
+	}
+	
+	private void addCheckLegalRead(AssignStmt read) {
+		InstanceFieldRef rightOp = (InstanceFieldRef) read.getRightOp();
+		Local base = (Local) rightOp.getBase();
+		Type baseType = base.getType();
+		
+		// Add locals
+		Local checkedTemp = J.newLocal("checkedTemp" + Integer.toString(tempLocalCount), Constants.CHECKED_CLASS.getType());
+		Local temp = J.newLocal("temp" + Integer.toString(tempLocalCount), baseType);
+		locals.add(checkedTemp);
+		locals.add(temp);
+		tempLocalCount++;
+		
+		// Insert the call to checkLegalRead
+		Unit checkStmt = J.newAssignStmt(checkedTemp, J.newStaticInvokeExpr(
+				Constants.CHECKED_CLASS.getMethod("checkLegalRead", Arrays.asList(new Type[] { Constants.CHECKED_CLASS.getType() })).makeRef(), 
+				Arrays.asList(new Local[] { base })));
+		units.insertBefore(checkStmt, read);
+		
+		// Insert a cast, to cast the result of checkLegalRead to the right type again
+		Unit castStmt = J.newAssignStmt(temp, J.newCastExpr(checkedTemp, baseType));
+		units.insertBefore(castStmt, read);
+		
+		// Change base of original field read to checked variable
+		read.setRightOp(J.newInstanceFieldRef(temp, rightOp.getFieldRef()));
+	}
+	
+	private void addCheckLegalWrite(AssignStmt write) {
+		InstanceFieldRef leftOp = (InstanceFieldRef) write.getLeftOp();
+		Local base = (Local) leftOp.getBase();
+		Type baseType = base.getType();
+		
+		Local checkedTemp = J.newLocal("checkedTemp" + Integer.toString(tempLocalCount), Constants.CHECKED_CLASS.getType());
+		Local temp = J.newLocal("temp" + Integer.toString(tempLocalCount), baseType);
+		locals.add(checkedTemp);
+		locals.add(temp);
+		tempLocalCount++;
+		
+		// Insert the call to checkLegalWrite
+		Unit checkStmt = J.newAssignStmt(checkedTemp, J.newStaticInvokeExpr(
+				Constants.CHECKED_CLASS.getMethod("checkLegalWrite", Arrays.asList(new Type[] { Constants.CHECKED_CLASS.getType() })).makeRef(), 
+				Arrays.asList(new Local[] { base })));
+		units.insertBefore(checkStmt, write);
+		
+		// Insert a cast, to cast the result of checkLegalRead to the right type again
+		Unit castStmt = J.newAssignStmt(temp, J.newCastExpr(checkedTemp, baseType));
+		units.insertBefore(castStmt, write);
+		
+		// Change base of original field read to checked variable
+		write.setLeftOp(J.newInstanceFieldRef(temp, leftOp.getFieldRef()));
+	}
+
+	private List<AssignStmt> findCheckedFieldReads() {
+		ArrayList<AssignStmt> result = new ArrayList<AssignStmt> ();
+		for (Unit u : units) {
+			// Because jimple uses 3-adress code, field reads are always a separate assign statement
+			if (u instanceof AssignStmt) {
+				AssignStmt a = (AssignStmt)u;
+				Value v = a.getRightOp();
+				if (v instanceof InstanceFieldRef) {
+					InstanceFieldRef f = (InstanceFieldRef)v;
+					Value base = f.getBase();
+					if (isSubtypeOfChecked(base.getType())) {
+						logger.debug(a + " is a checked field read!");
+						result.add(a);
+					}
+				}
+			}
+		}
+		return result;
+	}
+	
+	private List<AssignStmt> findCheckedFieldWrites() {
+		ArrayList<AssignStmt> result = new ArrayList<AssignStmt> ();
+		for (Unit u : units) {
+			// Because jimple uses 3-adress code, field reads are always a separate assign statement
+			if (u instanceof AssignStmt) {
+				AssignStmt a = (AssignStmt)u;
+				Value v = a.getLeftOp();
+				if (v instanceof InstanceFieldRef) {
+					InstanceFieldRef f = (InstanceFieldRef)v;
+					Value base = f.getBase();
+					if (isSubtypeOfChecked(base.getType())) {
+						logger.debug(a + " is a checked field write!");
+						result.add(a);
+					}
+				}
+			}
+		}
+		return result;
+	}
+	
+	/**
+	 * Returns <code>true</code> if <code>t</code> is a subtype of the class {@link rolez.checked.lang.Checked}
+	 * and <code>false</code> otherwise.
+	 * @param t
+	 * @return
+	 */
+	private boolean isSubtypeOfChecked(Type t) {
+		SootClass classOfType = Scene.v().loadClass(t.toString(), SootClass.HIERARCHY);
+		
+		// TODO: Add classes to scene
+		if (classOfType.isPhantom()) {
+			logger.debug("Class corresponding to type " + t + " is phantom class!");
+			throw new RuntimeException("Phantom class in application classes");
+		}
+		
+		SootClass currentClass = classOfType;
+		do {
+			if (currentClass.equals(Constants.CHECKED_CLASS)) {
+				return true;
+			}
+			currentClass = currentClass.getSuperclass();
+		} while(!currentClass.equals(Constants.OBJECT_CLASS));
+		return false;
 	}
 }
