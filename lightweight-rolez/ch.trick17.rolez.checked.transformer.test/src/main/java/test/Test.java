3e6bcd1790c446381035e582f8f3e38a101523c4
micgiger <micgiger@student.ethz.ch>
2017-08-26 14:51:40 +0200

    test for task call transformation

--- lightweight-rolez/ch.trick17.rolez.checked.transformer.test/src/main/java/test/Test.java
+++ lightweight-rolez/ch.trick17.rolez.checked.transformer.test/src/main/java/test/Test.java
@@ -1,17 +1,28 @@
 package test;
 
+import java.util.Random;
+
 import rolez.annotation.Checked;
+import rolez.annotation.Roleztask;
 
 @Checked
 public class Test {
 
+	B b;
+	
 	public static void main(String[] args) {
-		
+		Random r = new Random();
+		Test test = new Test();
+		test.test(true);
+		int i = 0;
+		i++;
 	}
 	
-	void test() {
+	@Roleztask
+	void test(final boolean $asTask) {
 		A a = new A();	// Checked object
 		a.foo = 2;
 		int i = a.foo;
+		System.out.println(i);
 	}
 }
