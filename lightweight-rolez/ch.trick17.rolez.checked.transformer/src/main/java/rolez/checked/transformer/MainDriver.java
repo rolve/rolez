a2c84afbde698fe429472e0ef22f1b219a116571
micgiger <micgiger@student.ethz.ch>
2017-08-29 17:02:08 +0200

    use program arguments for MainDriver

--- lightweight-rolez/ch.trick17.rolez.checked.transformer/src/main/java/rolez/checked/transformer/MainDriver.java
+++ lightweight-rolez/ch.trick17.rolez.checked.transformer/src/main/java/rolez/checked/transformer/MainDriver.java
@@ -1,6 +1,7 @@
 package rolez.checked.transformer;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
 
 import soot.G;
@@ -12,8 +13,14 @@ import soot.options.Options;
 public class MainDriver {
 	
 	public static void main(String[] args) {
+
+		// Get arguments
+		String processDir = args[0];	// Where are the classes to process located
+		String mainClass = args[1];		// What is the main class (fully qualified name)
+		String outputDirName = args[2];	// Custom output dir name -> output will be located in sootOutput/<outputDirName> folder
+		String outputFormat = args[3];	// J = jimple, C = class
 		
-		setUpSoot();
+		setUpSoot(processDir, mainClass, outputDirName, outputFormat);
 		
 		// Handles the @Roleztask and @Checked annotated classes and transforms them.
 		PackManager.v().getPack("wjtp").add(
@@ -30,22 +37,27 @@ public class MainDriver {
 			new Transform("jtp.transformer1", new CheckingTransformer())
 		);
 		
-        soot.Main.main(args);
+        soot.Main.main(new String[] {  });
         
         System.out.println("Finished transformation");
 	}
 	
-	private static void setUpSoot() {
+	private static void setUpSoot(String processDir, String mainClass, String outputDirName, String outputFormat) {
 		G.reset();
 		
 		Options opts = Options.v();
 		
 		opts.set_unfriendly_mode(true);
 		
-		// TODO: The process dir and main class should be an argument to the transformer
-		File f = new File("../ch.trick17.rolez.checked.transformer.test/target/classes");
-		opts.set_process_dir(Collections.singletonList(f.getAbsolutePath()));
-		opts.set_main_class("test.Test");
+		// Options depending on program arguments
+		File f = new File(processDir);
+		try {
+			opts.set_process_dir(Collections.singletonList(f.getCanonicalPath()));
+			opts.set_main_class(mainClass);
+			opts.set_output_dir("sootOutput/" + outputDirName);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		opts.set_soot_classpath("../ch.trick17.rolez.checked.lib/target/ch.trick17.rolez.checked.lib-1.0.0-SNAPSHOT.jar");
 		opts.set_prepend_classpath(true);
@@ -53,8 +65,10 @@ public class MainDriver {
 		opts.set_whole_program(true);
 		opts.setPhaseOption("cg", "enabled:false");
 		
-//		opts.set_output_format(Options.output_format_J);
-		opts.set_output_format(Options.output_format_class);
+		if (outputFormat.equals("J")) 
+			opts.set_output_format(Options.output_format_J);
+		if (outputFormat.equals("C"))
+			opts.set_output_format(Options.output_format_class);
 		
 		Scene.v().loadNecessaryClasses();
 	}
