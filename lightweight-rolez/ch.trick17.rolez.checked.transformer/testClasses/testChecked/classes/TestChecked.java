86b5eda8638486663d098127e3d6fb37e94ac4a6
micgiger <micgiger@student.ethz.ch>
2017-09-29 14:21:03 +0200

    a test which will test the Checked annotation

---
+++ lightweight-rolez/ch.trick17.rolez.checked.transformer/testClasses/testChecked/classes/TestChecked.java
@@ -0,0 +1,12 @@
+package classes;
+
+import rolez.annotation.Checked;
+import rolez.annotation.Roleztask;
+
+@Checked
+public class TestChecked {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
