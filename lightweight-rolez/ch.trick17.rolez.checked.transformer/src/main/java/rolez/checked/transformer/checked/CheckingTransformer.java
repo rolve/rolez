a2739d2479c410020b93aed8e9571221ed516f5b
micgiger <micgiger@student.ethz.ch>
2017-10-23 16:09:33 +0200

    remove unnecessary imports

--- lightweight-rolez/ch.trick17.rolez.checked.transformer/src/main/java/rolez/checked/transformer/checked/CheckingTransformer.java
+++ lightweight-rolez/ch.trick17.rolez.checked.transformer/src/main/java/rolez/checked/transformer/checked/CheckingTransformer.java
@@ -1,9 +1,7 @@
 package rolez.checked.transformer.checked;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
