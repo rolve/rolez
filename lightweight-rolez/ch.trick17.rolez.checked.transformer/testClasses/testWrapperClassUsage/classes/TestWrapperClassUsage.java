92d50334d5dfa28edae09301706939c6d207868a
micgiger <micgiger@student.ethz.ch>
2017-11-03 12:30:55 +0100

    make test more explicit

--- lightweight-rolez/ch.trick17.rolez.checked.transformer/testClasses/testWrapperClassUsage/classes/TestWrapperClassUsage.java
+++ lightweight-rolez/ch.trick17.rolez.checked.transformer/testClasses/testWrapperClassUsage/classes/TestWrapperClassUsage.java
@@ -4,24 +4,22 @@ import rolez.annotation.Checked;
 import rolez.annotation.Task;
 import rolez.annotation.Readwrite;
 
-import rolez.checked.util.Random;
-
 @Checked
 public class TestWrapperClassUsage {
 
-	Random rf;
+	rolez.checked.util.Random rf;
 	
 	public static void main(String[] args) {
 		TestWrapperClassUsage instance = new TestWrapperClassUsage();
-		instance.rf = new Random();
+		instance.rf = new rolez.checked.util.Random();
 		instance.rf.nextInt();
-		Random r = instance.rf;
-		if (r instanceof Random)
+		rolez.checked.util.Random r = instance.rf;
+		if (r instanceof rolez.checked.util.Random)
 			instance.task(r, true);
 	}
 	
 	@Task
-	void task(@Readwrite Random r, boolean $asTask) {
+	void task(@Readwrite rolez.checked.util.Random r, boolean $asTask) {
 		System.out.println("Hello world!");
 		java.util.Random jr = r.getUncheckedWriteInstance();
 		int i = javaUtilRandomMethod(jr);
