f9a3c4b5936ba3795b0b40993dc917d414a130e0
micgiger <micgiger@student.ethz.ch>
2017-11-03 15:17:31 +0100

    test checked array with objects... casts are really nasty...

---
+++ lightweight-rolez/ch.trick17.rolez.checked.transformer/testClasses/testCheckedArrayUsage/classes/A.java
@@ -0,0 +1,13 @@
+package classes;
+
+import rolez.annotation.Checked;
+
+@Checked
+public class A {
+
+	int i;
+	
+	A(int i) {
+		this.i = i;
+	}
+}
--- lightweight-rolez/ch.trick17.rolez.checked.transformer/testClasses/testCheckedArrayUsage/classes/TestCheckedArrayUsage.java
+++ lightweight-rolez/ch.trick17.rolez.checked.transformer/testClasses/testCheckedArrayUsage/classes/TestCheckedArrayUsage.java
@@ -13,28 +13,56 @@ public class TestCheckedArrayUsage {
 
 	public static void main(String[] args) {
 		TestCheckedArrayUsage instance = new TestCheckedArrayUsage();
-		CheckedArray<int[]> checkedArray = new CheckedArray<int[]>(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 } );
+		
+		// Test int array
+		CheckedArray<int[]> intArray = new CheckedArray<int[]>(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 } );
 		for (int i=0; i<4; i++) {
 			int begin = i*2;
 			int end = i*2 + 2;
-			CheckedSlice<int[]> slice = checkedArray.slice(begin, end);
-			instance.doubleArrayElements(slice, true);
+			CheckedSlice<int[]> slice = intArray.slice(begin, end);
+			instance.doubleIntArrayElements(slice, true);
 		}
+		instance.printIntArray(intArray, true);
 		
-		instance.printArray(checkedArray, true);
+		// Test object array
+		CheckedArray<A[]> objectArray = new CheckedArray<A[]>(new A[] { new A(1), new A(2) } );
+		for (int i=0; i<2; i++) {
+			int begin = i;
+			int end = i + 1;
+			CheckedSlice<A[]> slice = objectArray.slice(begin, end);
+			instance.negateArrayElementFields(slice, true);
+		}
+		instance.printObjectArray(objectArray, true);
 	}
 	
 	@Task
-	void doubleArrayElements(@Readwrite CheckedSlice<int[]> slice, boolean $asTask) {
+	void doubleIntArrayElements(@Readwrite CheckedSlice<int[]> slice, boolean $asTask) {
 		for (int i = slice.getSliceRange().begin; i < slice.getSliceRange().end; i++) {
 			slice.setInt(i, slice.getInt(i) * 2);
 		}
+		System.out.println("\n");
 	}
 	
 	@Task
-	void printArray(@Readonly CheckedArray<int[]> array, boolean $asTask) {
+	void printIntArray(@Readonly CheckedArray<int[]> array, boolean $asTask) {
 		for (int i = 0; i < array.arrayLength(); i++) {
 			System.out.print(array.getInt(i) + " ");
 		}
+	}
+	
+	@Task
+	void negateArrayElementFields(@Readwrite CheckedSlice<A[]> slice, boolean $asTask) {
+		for (int i = slice.getSliceRange().begin; i < slice.getSliceRange().end; i++) {
+			A a = slice.get(i);
+			a.i = -a.i;
+		}
+	}
+	
+	@Task
+	void printObjectArray(@Readonly CheckedArray<A[]> array, boolean $asTask) {
+		for (int i = 0; i < array.arrayLength(); i++) {
+			System.out.print(((A)array.get(i)).i + " ");
+		}
+		System.out.println("\n");
 	}
 }
