faefd296266fa8cdd6dadb09b0121c41513fe4f5
micgiger@student.etzh.ch <micgiger@student.ethz.ch>
2017-11-25 21:26:48 +0100

    montecarlo move all checks out of the loops

--- lightweight-rolez/ch.trick17.rolez.checked.transformer/testClasses/appMonteCarlo/classes/MonteCarloPath.java
+++ lightweight-rolez/ch.trick17.rolez.checked.transformer/testClasses/appMonteCarlo/classes/MonteCarloPath.java
@@ -11,23 +11,27 @@ class MonteCarloPath extends RatePath {
 	final double volatility;
 	final double expectedReturnRate;
 	
-	final CheckedArray<double[]> fluctuations;
+	final int steps;
+	
+	CheckedArray<double[]> fluctuations;
 	
 	public MonteCarloPath(Returns returns, int steps) {
 		super(returns, new CheckedArray<double[]>(new double[steps]));
 		this.volatility = returns.volatility;
 		this.expectedReturnRate = returns.expectedReturnRate;
-		double[] fArr = new double[steps];
-		this.fluctuations = new CheckedArray<double[]>(fArr); 
+		this.steps = steps;
 	}
 	
 	public void computeFluctuations(long randomSeed) {
 		Random random = new Random(randomSeed);
 		double mean = (this.expectedReturnRate - 0.5 * this.volatility * this.volatility) * this.dTime;
 		double stdDev = this.volatility * Math.sqrt(this.dTime);
-		for (int i = 0; i < this.fluctuations.arrayLength(); i++) {
-			this.fluctuations.setDouble(i, mean + stdDev * random.nextGaussian());
+		CheckedArray<double[]> fluctuations = new CheckedArray<double[]>(new double[steps]);
+		fluctuations.setDouble(0, 0);
+		for (int i = 0; i < fluctuations.arrayLength(); i++) {
+			fluctuations.setDouble(i, mean + stdDev * random.nextGaussian());
 		}
+		this.fluctuations = fluctuations;
 	}
 	
 	public void computePathValues(double startValue) {
@@ -35,7 +39,7 @@ class MonteCarloPath extends RatePath {
 		CheckedArray<double[]> fluctuations = this.fluctuations;
 		fluctuations.getDouble(0);
 		pathValues.setDouble(0, startValue);
-		for (int i = 1; i < this.pathValues.arrayLength(); i++) {
+		for (int i = 1; i < pathValues.arrayLength(); i++) {
 			pathValues.setDouble(i, pathValues.getDouble(i-1) * Math.exp(fluctuations.getDouble(i)));
 		}
 		this.pathValues = pathValues;
